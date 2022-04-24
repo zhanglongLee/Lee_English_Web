c54a5ee4bd58ffe1c6ddbc9785d3b89007784e98
dragonLee <944106568@qq.com>
2022-04-24 18:07:28 +0800

    update: 修改菜单名称

--- Admin-Code/src/config/stage/category.js
+++ Admin-Code/src/config/stage/category.js
@@ -1,33 +1,33 @@
 const categoryRouter = {
   route: null,
   name: null,
-  title: '分类管理',
+  title: '频道管理',
   type: 'folder', // 类型: folder, tab, view
   icon: 'iconfont icon-erji',
   filePath: 'view/category/', // 文件路径
   order: null,
   inNav: true,
-  permission: ['查询所有分类'],
+  permission: ['查询所有频道'],
   children: [
     {
-      title: '添加分类',
+      title: '添加频道',
       type: 'view',
       name: 'categoryCreate',
       route: '/category/add',
       filePath: 'view/category/category-create.vue',
       inNav: true,
       icon: 'iconfont icon-tushuguanli',
-      permission: ['新增分类'],
+      permission: ['新增频道'],
     },
     {
-      title: '分类列表',
+      title: '频道列表',
       type: 'view',
       name: 'categoryList',
       route: '/category/list',
       filePath: 'view/category/category-list.vue',
       inNav: true,
       icon: 'iconfont icon-tushuguanli',
-      permission: ['查询所有分类'],
+      permission: ['查询所有频道'],
     },
   ],
 }
